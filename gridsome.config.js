2fe643f625958dc004790741ba4b511669815553
Simon Duchaine <simon.duchaine@hotmail.com>
2019-09-27 13:36:17 -0400

    added GA tag

--- gridsome.config.js
+++ gridsome.config.js
@@ -35,6 +35,12 @@ module.exports = {
   },
   plugins: [
     {
+      use: "@gridsome/plugin-google-analytics",
+      options: {
+        id: "UA-38958579-4"
+      }
+    },
+    {
       use: "@gridsome/plugin-sitemap",
       options: {
         cacheTime: 600000 // default
