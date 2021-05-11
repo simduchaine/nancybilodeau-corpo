73f9b84e0885eb8cc92f1a008cbd99a35baf13b1
Simon Duchaine <simon.duchaine@hotmail.com>
2021-05-11 14:16:31 -0400

    added section bienfaits

--- src/pages/retraite-executive.vue
+++ src/pages/retraite-executive.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div style="margin-top:-3rem">
       <div
-        class="parallax-effect hero is-medium"
+        class="parallax-effect hero is-medium is-dark"
         style="background-image: url('/img/Retraite.png'); "
       >
         <div class="hero-body">
@@ -27,10 +27,7 @@
               affaires!
             </p>
             <g-image src="../../uploads/moins-de-40.png" />
-            <p>
-              Montrez-leur qu’ils sont importants pour vous et ils vous le
-              rendront!
-            </p>
+
             <p>
               Riche de mon expérience de fondatrice et gestionnaire du Centre
               Holistique Om Prana, je combine maintenant mes expériences
@@ -45,7 +42,7 @@
       </div>
 
       <div
-        class="parallax-effect hero is-medium"
+        class="parallax-effect hero is-medium is-dark"
         style="background-image: url('/img/duree.png'); "
       >
         <div class="hero-body has-text-centered">
@@ -77,9 +74,9 @@
           </p>
           <VueSlickCarousel
             :dots="false"
-            :arrows="false"
+            :arrows="true"
             :autoplay="true"
-            :autoplaySpeed="5000"
+            :autoplaySpeed="4000"
             :speed="2000"
             :infinite="true"
             :slidesToShow="3"
@@ -244,13 +241,14 @@
       </div>
 
       <div
-        class="parallax-effect hero is-medium"
+        class="parallax-effect hero is-medium is-dark"
         style="background-image: url('/img/retraite-repas.png'); "
       >
-        <div class="hero-body">
-          <h2 class="has-text-centered title is-family-primary is-size-2">
-            Repas inclus
+        <div class="hero-body has-text-centered">
+          <h2 class=" title is-family-primary is-size-2">
+            Repas santé
           </h2>
+          <h3 class="title">Simple ou gastronomique, planifié avec soins</h3>
         </div>
       </div>
 
@@ -268,13 +266,165 @@
       </div>
 
       <div
-        class="parallax-effect hero is-medium"
+        class="parallax-effect hero is-medium is-dark"
         style="background-image: url('/img/bienfaits.png'); "
       >
         <div class="hero-body">
           <h2 class="has-text-centered title is-family-primary is-size-2">
             Les bienfaits
           </h2>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="container">
+          <p>
+            Il peut être difficile au quotidien, et tout particulièrement en
+            télétravail, de développer l’esprit d’équipe, montrer votre
+            appréciation et renforcer la culture d’entreprise souhaitée. Un
+            changement de décor permet de briser la routine et offre une
+            occasion exceptionnelle de se concentrer sur des sujets donnés et de
+            voir les choses différemment tout en investissant dans la santé et
+            le mieux-être de vos employés. Voici les bénéfices généralement
+            recherchés :
+          </p>
+          <ul>
+            <li>Renforcement de l’esprit d’équipe</li>
+            <li>Amélioration des communications</li>
+            <li>Résolution de conflits</li>
+            <li>Mobilisation vers un ou des buts communs</li>
+            <li>Augmentation de la créativité</li>
+            <li>Gestion mature de l’égo</li>
+            <li>Appréciation de la direction démontrée de façon tangible</li>
+            <li>
+              Personnel inspiré à prendre soin d’eux pour offrir le meilleur
+              d’eux même
+            </li>
+            <li>Amélioration du sentiment d’appartenance à la compagnie</li>
+            <li>
+              Augmentation de la capacité d’adaptation dans d’un monde en
+              ébullition
+            </li>
+            <li>Correction des dysfonctions de l’équipe</li>
+            <li>
+              Rétablissement d’un climat de travail agréable et en confiance
+            </li>
+            <li>Transformation réelle des leaders et de la culture</li>
+            <li>BOOST de performance</li>
+            <li>RESET!</li>
+          </ul>
+          <p>
+            On peut accomplir plus en deux jours bien structurés qu’en des
+            heures de meeting au bureau ou chacun continue d’opérer selon les
+            mêmes principes et paradigmes qui ont mené aux résultats actuels.
+          </p>
+          <h3 class="title is-size-6">Exemple de Retraite Exécutive</h3>
+          <ul>
+            <li>Client : Kerry Usine de Ste-Claire</li>
+            <li>Nombre de participants : 10 participants</li>
+            <li>Durée : 2 nuits, 2 jours</li>
+            <li>Lieu : Monastère des Augustines</li>
+            <li>Thème : Team Upgrade!</li>
+            <li>
+              Repas : Menu santé pré sélectionné préparé et servi par le bistro
+              du Monastère des Augustines
+            </li>
+            <li>
+              Objectifs :
+              <ul>
+                <li>Communiquer l’appréciation sincère pour tous et chacun.</li>
+                <li>Reconnaître la réalité du changement continue.</li>
+                <li>
+                  Prendre un moment d’arrêt et de réflexion sur les essentiels
+                  au succès de l’équipe.
+                </li>
+                <li>
+                  Aligner le portefeuille de projet correctement avec les
+                  objectifs annuels.
+                </li>
+                <li>
+                  Améliorer la communication et les échanges entre les membres
+                  de l’équipe.
+                </li>
+                <li>
+                  Trouver des moyens efficaces pour rester aligné tout au cours
+                  de l’année.
+                </li>
+                <li>
+                  Développer le sens d’appartenance à l’usine au-delà des lignes
+                  de supervision.
+                </li>
+                <li>
+                  Apprendre à communiquer de façon authentique, respectueuse et
+                  axé sur l’avancement et la résolution des problèmes.
+                </li>
+                <li>Améliorer la gestion de l’égo.</li>
+                <li>Vaincre les 5 dysfonctions d’une équipe.</li>
+                <li>Vivre un bon moment d’appréciation et d’ouverture.</li>
+              </ul>
+            </li>
+          </ul>
+
+          <hr />
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-128x128">
+                <g-image
+                  class="is-rounded"
+                  src="../../uploads/Jimmy.png"
+                ></g-image>
+              </figure>
+            </div>
+
+            <div class="media-content">
+              <div>
+                <p>
+                  Ayant collaboré et travaillé avec Nancy à plusieurs occasions
+                  dans le passé, je n’avais aucun doute que notre expérience
+                  serait gagnante, et ce fût au-delà de mes attentes et celles
+                  de mon équipe.
+                </p>
+                <p>
+                  Lors d’une activité récente de retraite et team building avec
+                  mon équipe de gestionnaires, nous étions en quête d’une
+                  expérience qui nous permettrait à la fois de nous recentrer
+                  afin de travailler plus efficacement, mais aussi une occasion
+                  pour prendre soins de nous tous, l’humain, derrière chaque
+                  gestionnaire, ce qui ne se fait malheureusement pas assez
+                  souvent en entreprise.
+                </p>
+                <p>
+                  C’est une activité très hors du commun, qui demande de
+                  l’audace et qui sort du cadre ‘’normal’’ de formation/cadre de
+                  gestion courante, mais je vous le dis, c’est le meilleur
+                  investissement que vous pouvez faire, prêtez-vous au jeu et à
+                  l’expérience.
+                </p>
+                <p>
+                  Nancy a définitivement fait de nous une équipe renouvelée,
+                  soudée plus que jamais, ce qui a contribué a augmenté
+                  significativement l’efficacité globale de l’équipe. Le mix
+                  entre l’approche plus conventionnelle de fonctionnement d’une
+                  équipe (Lancioni, les 5 dysfonctions d’une équipe) enseigné,
+                  et les techniques de cohérence appliquées et maîtrisées par
+                  Nancy ont résulté en un cocktail gagnant. Le changement est
+                  radical, et les résultats sont tangibles!
+                </p>
+                <p>
+                  Merci Nancy, ce fût une de mes meilleures expériences
+                  professionnelles jusqu’à maintenant, et je tiens à dire qu’en
+                  tant que dirigeant d’entreprise, c’est quelque chose que nous
+                  devrions tous prendre le temps de faire, plus souvent! Osez!
+                </p>
+              </div>
+              <h2 class="title is-size-6 is-family-primary">
+                Jimmy
+                <span>, Kerry</span>
+              </h2>
+            </div>
+          </div>
+          <hr />
+          <h4 class="title is-size-6">Contactez-moi pour en savoir plus!</h4>
         </div>
       </div>
 
@@ -287,7 +437,7 @@
           </p>
           <VueSlickCarousel
             :dots="false"
-            :arrows="false"
+            :arrows="true"
             :autoplay="true"
             :autoplaySpeed="4000"
             :speed="2000"
@@ -400,39 +550,45 @@
           </p>
           <div class="columns">
             <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <g-image src="../../uploads/Formation-Corporative-2.png" />
-                  </figure>
+              <g-link to="/formations">
+                <div class="card">
+                  <div class="card-image">
+                    <figure class="image is-4by3">
+                      <g-image
+                        src="../../uploads/Formation-Corporative-2.png"
+                      />
+                    </figure>
+                  </div>
                 </div>
-              </div>
+              </g-link>
             </div>
             <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <g-image src="../../uploads/conferences-corporatives.png" />
-                  </figure>
+              <g-link to="/conference">
+                <div class="card">
+                  <div class="card-image">
+                    <figure class="image is-4by3">
+                      <g-image
+                        src="../../uploads/conferences-corporatives.png"
+                      />
+                    </figure>
+                  </div>
                 </div>
-              </div>
+              </g-link>
             </div>
             <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-4by3">
-                    <g-image src="../../uploads/formations-individu.png" />
-                  </figure>
+              <a href="https://coaching.nancybilodeau.com/formations">
+                <div class="card">
+                  <div class="card-image">
+                    <figure class="image is-4by3">
+                      <g-image src="../../uploads/formations-individu.png" />
+                    </figure>
+                  </div>
                 </div>
-              </div>
+              </a>
             </div>
           </div>
           <g-image src="../../uploads/seulement-42-pourcent.png" />
 
-          <p class="has-text-centered">
-            Montrez-leur votre appréciation et offrez-leur une expérience unique
-            qui les aidera dans toutes les sphères de leur vie.
-          </p>
           <p>
             Contactez-moi pour en savoir plus!
           </p>
@@ -481,5 +637,9 @@ export default {
   .card {
     box-shadow: none;
   }
+}
+.slick-prev:before,
+.slick-next:before {
+  color: #363636;
 }
 </style>
